231164cac878be59057d3044d2e09d254242c58a
HoJunPark <mara8534@naver.com>
2020-02-19 14:04:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 import os
 import re
-import sys
-import uuid
 
 import tensorflow as tf
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 from settings import PROJECT_ROOT
 from chatbot.botpredictor import BotPredictor
@@ -15,25 +13,32 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 corp_dir = os.path.join(PROJECT_ROOT, 'Data', 'Corpus')
 knbs_dir = os.path.join(PROJECT_ROOT, 'Data', 'KnowledgeBase')
 res_dir = os.path.join(PROJECT_ROOT, 'Data', 'Result')
+
+NOT_UNDERSTOOD = 'pardon?'
+
 with tf.Session() as sess:
     predictor = BotPredictor(sess, corpus_dir=corp_dir, knbase_dir=knbs_dir,
                             result_dir=res_dir, result_file='basic')
 
-    @app.route('/question', methods=['GET', 'POST'])
+    @app.route('/chatbot', methods=['GET', 'POST'])
     def Question():
-        session_id = 0
         try:
-            session_id = int(request.args.get('session_id', 0))
+            session_id = int(request.args.get('previous_session_id', 1))
             predictor.session_data.get_session(session_id)
         except Exception as e:
             print(e)
             session_id = predictor.session_data.add_session()
         
         question_sentence = request.args.get('sentence', '')
+        answer = NOT_UNDERSTOOD
         if question_sentence:
-            print(question_sentence)
-            return re.sub(r'_nl_|_np_', ' ', predictor.predict(session_id, question_sentence)).strip()
-        return 'pardon?'
+            answer = re.sub(r'_nl_|_np_', ' ', predictor.predict(session_id, question_sentence)).strip()
+        return jsonify({
+            'question': question_sentence,
+            'answer': answer,
+            'current_session_id': session_id,
+            'okay': answer != NOT_UNDERSTOOD,
+        })
 
     if __name__ == "__main__":
         app.run(host='0.0.0.0', port=80, debug=True)
